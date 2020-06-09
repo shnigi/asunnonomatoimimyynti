92f0008e2cce642ee2dcf4e62f398e6febd95cce
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-06-09 14:10:54 +0300

    remove code

--- src/pages/referenssit.js
+++ src/pages/referenssit.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 import { Helmet } from 'react-helmet'
 import Gallery from 'react-photo-gallery'
@@ -153,131 +152,6 @@ const photos2 = [
     height: 2
   }
 ];
-// const photos = [
-//   {
-//     src: 'referenssit/asunto1/asunto1.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto2.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto3.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto4.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto5.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto6.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto7.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto8.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto9.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto10.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto11.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto1/asunto12.jpg',
-//     width: 4,
-//     height: 2
-//   }
-// ];
-
-// const photos2 = [
-//   {
-//     src: 'referenssit/asunto2/asunto1.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto2.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto3.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto4.jpg',
-//     width: 2,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto5.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto6.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto7.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto8.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto9.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto10.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto11.jpg',
-//     width: 4,
-//     height: 2
-//   },
-//   {
-//     src: 'referenssit/asunto2/asunto12.jpg',
-//     width: 4,
-//     height: 2
-//   }
-// ];
 
 export const ReferenssittPageTemplate = ({helmet}) => {
   return (
@@ -326,14 +200,3 @@ const ReferenssitPage = () => {
 }
 
 export default ReferenssitPage
-
-// export const HinnastoPageQuery = graphql`
-//   query HinnastoPage($id: String!) {
-//     markdownRemark(id: { eq: $id }) {
-//       frontmatter {
-//         title
-//         description
-//       }
-//     }
-//   }
-// `
