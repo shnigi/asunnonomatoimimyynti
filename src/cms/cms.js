c3fbefafda92f210f39d7ae367a5c33bb328e661
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-06-09 14:34:49 +0300

    updates

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,9 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
-
-// import HinnastoPagePreview from './preview-templates/HinnastoPagePreview'
-// import ProductPagePreview from './preview-templates/ProductPagePreview'
-// import IndexPagePreview from './preview-templates/IndexPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-// CMS.registerPreviewTemplate('index', IndexPagePreview)
-// CMS.registerPreviewTemplate('hinnasto', HinnastoPagePreview)
-// CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
