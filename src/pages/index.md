f801ac542cb70e440a47fa2ccde77da8c7cb5132
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-06-16 14:10:14 +0300

    control content

--- src/pages/index.md
+++ src/pages/index.md
@@ -22,4 +22,22 @@ mainpitch: >-
 
 
   Lisämaksusta otamme myös ilmakuvat ja syötämme tiedot haluamiisi internetin myyntikanaviin, kuten Etuovi.com, Tori.fi ja Oikotie.
+secondpitch: >-
+    Asuntomyynnissä tärkeintä on löytää potentiaalinen ostaja. Niitä tarvitaan vain yksi. Kun sinulla on hyvin tehdyt myynti-ilmoitukset ja esitteet, niin ostajat löytävät kohteesi. Hyvä ja hyvin markkinoitu kohde myy melkein itse itsensä.
+
+
+    Sinun tehtäväsi on määrittää hinta, pyytää isännöitsijältä Isännöitsijäntodistus, Energiatodistus, Tilinpäätös, Korjaussuunnitelma sekä Pohjapiirustus. Ota sen jälkeen yhteys Asunnon omatoimimyyntiin ja sovi kuvaus/esittelypäivä. Saat sen jälkeen viikon sisällä tarvitsemasi materiaalit sähköpostiisi.
+
+
+    Asunnon omatoimimyynti
+    Lisämaksusta postitamme sinulle myyntiesitteitä haluamasi määrän ja laadimme myynti-ilmoitukset haluamiisi kanaviin.
+
+
+    Sen jälkeen sinun ei tarvitse muuta kuin odottaa potentiaalisten ostajien yhteydenottoa ja sopia näytöistä. Tarjoukset voit ottaa vastaan esimerkiksi Asunnon omatoimimyynnistä saamallasi lomakkeella. Kauppakirja ja kaupathan tehdään yleensä ostajan pankissa. Sitä ennen sinun tulee olla hyvissä ajoin yhteydessä omaan pankkiisi, jossa asuntosi saattaa olla esimerkiksi lainan vakuutena.
+
+
+    Kiinteistövälittäjät väittävät olevansa ammattilaisia viestinnässä, kuvaamisessa, myymisessä ja lakiasioissa. Heillä on omien sanojensa mukaan yleensä jo valmiina myös laaja ostajapotentiaali. Jos uskot heidän olevan näin ylivertaisia, niin varaudu maksamaan jopa kymppitonnin palkkio. Jos sen sijaan arvelet tuntevasi itse paremmin oman asuntosi hyvät puolet ja ominaisuudet ja luotat markkinointiviestinnän ammattilaisen tuottamaan materiaaliin, niin myy itse! Älä siis heitä rahojasi kankkulan kaivoon, vaan ota yhteys Asunnon omatoimimyyntiin!
+lastheading: Rohkeasti vaan toimeen!
+thirdpitch: >-
+    Jokainen meistä osaa myydä! Korosta hyviä puolia, mutta ole myös rehellinen! Kuuntele ennemmin asiakasta ja vastaa kysyttäessä. Liian innokas myyminen on yleensä vain haitaksi. Kun sinulla on hyvää myyntimateriaalia tukenasi, puheet voi jättää vähemmälle. Kysy ihmeessä myös välittäjältä paljonko hän ottaa myyntipalkkiota ja mitä kaikkea sillä saat. Vertaa sen jälkeen hintoja ja laske erotus, jonka tulet säästämään! Ota siis rohkeasti yhteyttä Asunnon omatoimimyyntiin!
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -12,7 +12,10 @@ export const IndexPageTemplate = ({
   helmet,
   subImage,
   secondheading,
-  mainpitch
+  mainpitch,
+  secondpitch,
+  lastheading,
+  thirdpitch
 }) => (
   <div>
       {helmet || ''}
@@ -43,7 +46,7 @@ export const IndexPageTemplate = ({
         <div className="inner split">
           <section>
             <h2>Asunnon omatoimisella myynnillä säästät tuhansia euroja!</h2>
-            <p>{MarkdownContent(mainpitch)}</p>
+            {MarkdownContent(mainpitch)}
             <ul className="actions action-buttonwrapper">
               <li><a href="#asuntomyynti" className="button">Lue lisää</a></li>
             </ul>
@@ -78,44 +81,15 @@ export const IndexPageTemplate = ({
 
       <section className="wrapper style2 alt">
         <div className="container">
-        <p>Asuntomyynnissä tärkeintä on löytää potentiaalinen ostaja. Niitä tarvitaan vain yksi. Kun sinulla on hyvin
-        tehdyt myynti-ilmoitukset ja esitteet, niin ostajat löytävät kohteesi. Hyvä ja hyvin markkinoitu kohde myy
-        melkein itse itsensä.</p>
-        <p>
-        Sinun tehtäväsi on määrittää hinta, pyytää isännöitsijältä Isännöitsijäntodistus, Energiatodistus, Tilinpäätös,
-        Korjaussuunnitelma sekä Pohjapiirustus. Ota sen jälkeen yhteys Asunnon omatoimimyyntiin ja sovi
-        kuvaus/esittelypäivä. Saat sen jälkeen viikon sisällä tarvitsemasi materiaalit sähköpostiisi.
-        </p>
           <img src={logo} alt="Asunnon omatoimimyynti" className="frontpage-section-logo" />
-        <p>
-        Lisämaksusta postitamme sinulle myyntiesitteitä haluamasi määrän ja laadimme myynti-ilmoitukset
-        haluamiisi kanaviin.
-        </p>
-        <p>
-        Sen jälkeen sinun ei tarvitse muuta kuin odottaa potentiaalisten ostajien yhteydenottoa ja sopia näytöistä.
-        Tarjoukset voit ottaa vastaan esimerkiksi Asunnon omatoimimyynnistä saamallasi lomakkeella. Kauppakirja
-        ja kaupathan tehdään yleensä ostajan pankissa. Sitä ennen sinun tulee olla hyvissä ajoin yhteydessä omaan
-        pankkiisi, jossa asuntosi saattaa olla esimerkiksi lainan vakuutena.
-        </p>
-        <p>
-        Kiinteistövälittäjät väittävät olevansa ammattilaisia viestinnässä, kuvaamisessa, myymisessä ja lakiasioissa.
-        Heillä on omien sanojensa mukaan yleensä jo valmiina myös laaja ostajapotentiaali. Jos uskot heidän olevan
-        näin ylivertaisia, niin varaudu maksamaan jopa kymppitonnin palkkio. Jos sen sijaan arvelet tuntevasi itse
-        paremmin oman asuntosi hyvät puolet ja ominaisuudet ja luotat markkinointiviestinnän ammattilaisen
-        tuottamaan materiaaliin, niin myy itse! Älä siis heitä rahojasi kankkulan kaivoon, vaan ota yhteys Asunnon
-        omatoimimyyntiin!
-        </p>
+          {MarkdownContent(secondpitch)}
         </div>
       </section>
 
       <section id="asuntomyynti" className="wrapper">
         <div className="container">
-          <h2>Rohkeasti vaan toimeen!</h2>
-          <p>Jokainen meistä osaa myydä! Korosta hyviä puolia, mutta ole myös rehellinen! Kuuntele ennemmin
-          asiakasta ja vastaa kysyttäessä. Liian innokas myyminen on yleensä vain haitaksi. Kun sinulla on hyvää
-          myyntimateriaalia tukenasi, puheet voi jättää vähemmälle. Kysy ihmeessä myös välittäjältä paljonko hän
-          ottaa myyntipalkkiota ja mitä kaikkea sillä saat. Vertaa sen jälkeen hintoja ja laske erotus, jonka tulet
-          säästämään! Ota siis rohkeasti yhteyttä Asunnon omatoimimyyntiin!</p>
+        <h2>{lastheading}</h2>
+          {MarkdownContent(thirdpitch)}
           <ul className="actions action-buttonwrapper">
             <li><Link to="/otayhteytta" className="button">Ota yhteyttä</Link></li>
           </ul>
@@ -126,8 +100,6 @@ export const IndexPageTemplate = ({
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
-  console.log('data', data);
-  
   return (
     <Layout>
       <IndexPageTemplate
@@ -140,6 +112,9 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
         secondheading={frontmatter.secondheading}
         mainpitch={frontmatter.mainpitch}
+        secondpitch={frontmatter.secondpitch}
+        lastheading={frontmatter.lastheading}
+        thirdpitch={frontmatter.thirdpitch}
         helmet={
           <Helmet>
             <title>{frontmatter.title}</title>
@@ -185,6 +160,9 @@ export const pageQuery = graphql`
         keywords
         secondheading
         mainpitch
+        secondpitch
+        lastheading
+        thirdpitch
       }
     }
   }
